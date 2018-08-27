12991f48e799659feb8a89ef577073646de65cc8
juan arciniega <juanarciniega@juans-iMac.local>
2018-08-27 09:28:32 -0700

    added angularfire authentication

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs/Subject';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 import { User } from './user.model';
 import { AuthData } from './auth-data.model';
@@ -16,22 +17,34 @@ export class AuthService {
 	// by making this a private field, we can only access it form inside this service
 	private user: User;
 
-	constructor(private router: Router) {}
+	constructor(private router: Router, private afAuth: AngularFireAuth) {}
 
 	// on register or login turn user into an object
 	registerUser(authData: AuthData){
-		this.user = {
-			email: authData.email,
-			userId: Math.round(Math.random() * 10000).toString()
-		};
+		this.afAuth.auth.createUserWithEmailAndPassword(
+			authData.email, 
+			authData.password
+		).then(result => {
+			console.log(result);
+			this.authSuccessfully();
+		})
+		.catch( error => {
+			console.log(error);
+		})
 		this.authSuccessfully('/training');
 	}
 
 	login(authData: AuthData){
-		this.user = {
-			email: authData.email,
-			userId: Math.round(Math.random() * 10000).toString()
-		};
+		this.afAuth.auth.signInWithEmailAndPassword(
+			authData.email,
+			authData.password
+		).then(result => {
+			console.log(result);
+			this.authSuccessfully();
+		})
+		.catch( error => {
+			console.log(error);
+		})
 		this.authSuccessfully('/training');
 	}
 
